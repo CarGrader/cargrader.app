ce26b3fbf6c04d65b77cc2d1200cd4a925cd681a
CarGrader <cargrader@gmail.com>
2025-08-26 22:13:22 -0700

    Update config.py
    Update for indendation error

--- cargrader.app/config.py
+++ cargrader.app/config.py
@@ -2,9 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-    ENV = os.environ.get("FLASK_ENV", "production")
-    DEBUG = ENV == "development"
+    DB_PATH = os.environ.get("DB_PATH") or "/opt/render/project/src/data/GraderRater.db"
 
-    # SQLite URI like: file:path?mode=ro&cache=shared (we’ll centralize connection in app/db/connection.py)
-   DB_PATH = os.environ.get("DB_PATH") or "/opt/render/project/src/data/GraderRater.db"
+
 
